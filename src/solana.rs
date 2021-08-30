6b14dea325ffbdca737d22f27bb4705fea29710b
Sushant Chandla <sushantchandla@gmail.com>
2021-08-30 23:35:36 +0530

    added program

--- src/solana.rs
+++ src/solana.rs
@@ -5,10 +5,7 @@ use solana_sdk::account::Account;
 pub fn get_all_account() -> Vec<(Pubkey, Account)> {
     let url = "https://api.devnet.solana.com".to_string();
     let client = RpcClient::new(url);
-    let pubkey = Pubkey::new(&[
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1,
-    ]);
+    let pubkey = Pubkey::new("7ZRbUhE2WU57qXaGWZ9hJ6rqf1PvuGJT2LEzRwPvho1x".as_bytes());
     let res = client.get_program_accounts(&pubkey).unwrap();
     res
 }
