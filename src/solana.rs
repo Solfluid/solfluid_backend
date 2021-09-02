43061bc80f3310536893119e5f88a0ab83181f40
Sushant Chandla <sushantchandla@gmail.com>
2021-09-02 18:13:52 +0530

    updated contract address

--- src/solana.rs
+++ src/solana.rs
@@ -8,7 +8,7 @@ pub fn get_all_account() -> Vec<(Pubkey, Account)> {
     let url = "https://api.devnet.solana.com".to_string();
     let client = RpcClient::new(url);
 
-    let pubkey = Pubkey::from_str("E55x1eejnQF4W67QanrZkihsCECyH2YdNDujXTunSQF2").unwrap();
+    let pubkey = Pubkey::from_str("GZLs51zidwmTGiftGJt1in3kWaicd4Ctvp9W95hZxgiZ").unwrap();
     let res = client.get_program_accounts(&pubkey).unwrap();
     res
 }
@@ -17,4 +17,9 @@ pub fn get_rent_exemption() -> u64 {
     let url = "https://api.devnet.solana.com".to_string();
     let client = RpcClient::new(url);
     client.get_minimum_balance_for_rent_exemption(97).unwrap()
+}
+
+
+pub fn stake(){
+    
 }
